b0f41cdb60ca5f65ff3b73e54e29ff87657be226
Dedi ir <135270896+ded23579@users.noreply.github.com>
2025-04-03 14:30:44 +0700

    Update articles.js

--- assets/articles.js
+++ assets/articles.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function loadArticles() {
-    const sheetUrl = 'https://api.dewan36912.workers.dev/'; // Ganti dengan URL API
+    const sheetUrl = 'https://script.google.com/macros/s/AKfycbzoZFeiNRnSBu9ou96JoH63RzKjNhFRUHke5FlrpCn081pwTvG4crBnlDZhIFTRuGer/exec'; // Ganti dengan URL API
     console.log("Fetching from URL:", sheetUrl); // Debugging
 
     fetch(sheetUrl)
