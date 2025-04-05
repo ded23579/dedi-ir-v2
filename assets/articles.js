654a29a38e3737a8f9191162a1cad9dac36d3ee4
Dedi ir <135270896+ded23579@users.noreply.github.com>
2025-04-05 10:00:59 +0700

    Update articles.js

--- assets/articles.js
+++ assets/articles.js
@@ -2,8 +2,14 @@ function loadArticles() {
     const sheetUrl = 'https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLhEgKIC0a-Bb2lQHBceyp2qfyrZQtAAbYjXqeHI5ab-ydZQ6sUtbu0wEK_v1axVNVkdYDtsWsbB0wSPaqJ8TsaYo4J28Phc9bus5IoXmq6OckECLDmzgJV_P_re91O73-f5ERWNLSeeoi15mgw1_ekZ8fgjM3UjI1qvexap92zq6GHrXXgNEo1vkwqRlwpDstvr4UfYb6IQHmNsiwY9ZXB6mSYfMPvnWjoW0gC-GBDNs_AUrOnyRmQbKZSakXr6Clf0hbJzAoCFwEuoxXAlbgKIv-cxCA&lib=MgnNHUJ8nyHtCMfKhbjqgCUf_rrIVLfIM'; // Ganti dengan URL publik Google Sheets Anda
 
     fetch(sheetUrl)
-        .then(response => response.text())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.text();
+        })
         .then(csvText => {
+            console.log("CSV Text:", csvText);
             const rows = csvText.split('\n').map(row => row.split(','));
             const articles = rows.slice(1).map(row => row.slice(2)); // Hilangkan header
             
@@ -47,5 +53,6 @@ function loadArticles() {
 }
 
 function showFullArticle(url) {
+    console.log("Navigating to URL:", url);
     window.location.href = url;
 }
